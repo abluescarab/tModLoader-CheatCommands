9a087c9a179f357c2ae2b69122d4811b6eb6aa96
Alana Gilston <abluescarab@users.noreply.github.com>
2017-12-05 16:52:36 -0800

    Made command change time instead of get time

--- Commands/World/Time.cs
+++ Commands/World/Time.cs
@@ -1,57 +1,106 @@
-﻿using Terraria;
-using Terraria.Localization;
+﻿using System.Text.RegularExpressions;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace CheatCommands.Commands.World {
     class Time : CheatCommand {
         public override string CommandName => "Time";
-        public override string Command => Language.GetTextValue("CLI.Time_Command");
-        public override string Description => "Get the world time.";
-        public override int MinimumArguments => 0;
+        public override string Command => "time";
+        public override string Usage => base.Usage + " <dawn/dusk/noon/midnight/time>";
+        public override string Description => "Change the time of day.";
+        public override int MinimumArguments => 1;
 
-        // from Terraria server commands
         public override void Action(CommandCaller caller, string[] args) {
-            string ampm = "AM";
-            double time = Main.time;
-            double num = 7.5;
+            int hours = 0;
+            int minutes = 0;
+            bool succeeded = true;
 
-            if(!Main.dayTime) {
-                time += 54000.0;
+            switch(args[0]) {
+                case "dawn":
+                    ToTwentyFourHourTime("4:30 AM", out hours, out minutes);
+                    break;
+                case "noon":
+                    ToTwentyFourHourTime("12:00 PM", out hours, out minutes);
+                    break;
+                case "dusk":
+                    ToTwentyFourHourTime("7:30 PM", out hours, out minutes);
+                    break;
+                case "midnight":
+                    ToTwentyFourHourTime("12:00 AM", out hours, out minutes);
+                    break;
+                default:
+                    succeeded = ToTwentyFourHourTime(args[0], out hours, out minutes);
+                    break;
+            }
+            
+            if(succeeded) {
+                ToTicks(hours, minutes);
+            }
+            else {
+                throw new UsageException("Invalid time format: " + args[0]);
+            }
+        }
+
+        private static bool ToTwentyFourHourTime(string timeString, out int hours, out int minutes) {
+            Regex twentyFourHour = new Regex(@"^([01]?\d|2[0-3]):([0-5]\d)");
+            Regex twelveHour = new Regex(@"^(1[0-2]|0?\d):([0-5]\d) ([AaPp][Mm])");
+
+            Match twentyFourHourMatch = twentyFourHour.Match(timeString);
+            Match twelveHourMatch = twelveHour.Match(timeString);
+
+            int finalHours = 0;
+
+            if(!twentyFourHourMatch.Success && !twelveHourMatch.Success) {
+                hours = 0;
+                minutes = 0;
+                return false;
             }
 
-            time = time / 86400.0 * 24.0;
-            time = time - num - 12.0;
+            if(twelveHourMatch.Success) {
+                finalHours = int.Parse(twelveHourMatch.Groups[1].Value);
+                bool am = twelveHourMatch.Groups[3].Value.ToLower().Equals("am");
 
-            if(time < 0.0) {
-                time += 24.0;
+                if(am && finalHours == 12) {
+                    finalHours -= 12;
+                }
+                else if(!am && finalHours < 12) {
+                    finalHours += 12;
+                }
+            }
+            else if(twentyFourHourMatch.Success) {
+                finalHours = int.Parse(twentyFourHourMatch.Groups[1].Value);
             }
 
-            if(time >= 12.0) {
-                ampm = "PM";
+            hours = finalHours;
+            minutes = int.Parse(twentyFourHourMatch.Groups[2].Value);
+            return true;
+        }
+
+        private static void ToTicks(double hours, double minutes) {
+            bool dayTime = false;
+            double time = 0.0;
+
+            minutes = minutes / 60.0;
+            time = hours + minutes;
+
+            if(time >= 4.5 && time < 19.5) {
+                dayTime = true;
             }
 
-            int hours = (int)time;
-            double minutes = time - hours;
+            time = time + 7.5 + 12.0;
 
-            minutes = (int)(minutes * 60.0);
-
-            string minutesString = minutes.ToString();
-
-            if(minutes < 10.0) {
-                minutesString = "0" + minutesString;
+            if(time >= 24.0) {
+                time -= 24.0;
             }
 
-            if(hours > 12) {
-                hours -= 12;
+            time = time * 86400.0 / 24.0;
+
+            if(!dayTime && time >= 54000.0) {
+                time -= 54000.0;
             }
 
-            if(hours == 0) {
-                hours = 12;
-            }
-
-            caller.Reply(Language.GetTextValue("CLI.Time", string.Concat(new object[] {
-                hours, ":", minutesString, " ", ampm
-            })));
+            Main.dayTime = dayTime;
+            Main.time = time;
         }
     }
 }
