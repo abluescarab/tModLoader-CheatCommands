6acb0ed36b66d5d6ca7075ac6e1e4ae89513241c
Alana Gilston <abluescarab@users.noreply.github.com>
2023-03-13 09:47:00 -0700

    Removed list of commands

--- CheatCommands.cs
+++ CheatCommands.cs
@@ -1,31 +1,7 @@
-﻿using CheatCommands.Commands;
-using CheatCommands.Commands.NPCs;
-using CheatCommands.Commands.Player;
-using CheatCommands.Commands.Player.Teleport;
-using CheatCommands.Commands.World;
-using System.Collections.Generic;
-using Terraria.ModLoader;
+﻿using Terraria.ModLoader;
 
 namespace CheatCommands {
     class CheatCommands : Mod {
-        private static readonly List<CheatCommand> Commands = new List<CheatCommand>() {
-            new KillAll(),
-            new KillNPC(),
-            new SpawnNPC(),
-            new Buff(),
-            new RemoveBuff(),
-            new GiveCoins(),
-            new GiveItem(),
-            new GodMode(),
-            new InfiniteAmmo(),
-            new Respawn(),
-            new Teleport(),
-            new FreezeTime(),
-            new SetSpawn(),
-            new SettleLiquids(),
-            new Time()
-        };
-
         public CheatCommands() {
             ContentAutoloadingEnabled = true;
         }
